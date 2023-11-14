018c383f2de3ad1fd839d6569db3f1195fd916bb
Tyler Sheffield <tylersheffield427@gmail.com>
2023-11-13 17:07:46 -0700

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let express = require("express");
+
+let app = express();
+
+let path = require("path");
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
+
+app.get("/login", (req,res))
