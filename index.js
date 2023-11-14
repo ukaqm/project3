9b5bc3d0f631c0ec7e14a98a915e782fc2d1c242
afoote99 <afoote99@gmail.com>
2023-11-14 01:58:45 -0700

    Login Page + reformatted
    Created the start to a login page and reformatted files so that it runs smoothly with node and express.

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ let app = express();
 
 let path = require("path");
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
+const port = 3000;
 
-app.get("/login", (req, res))
+app.use(express.urlencoded({ extended: true}));
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.get("/login", (req, res) =>
+    res.render("login"))
+
+app.listen(port, () => console.log("Server is running"))
