e8ab5b4d3441b3dabc09e7ef2ae92e6f4cabc90a
Tyler Sheffield <tylersheffield427@gmail.com>
2023-11-25 15:32:16 -0700

    commit

--- index.js
+++ index.js
@@ -10,9 +10,45 @@ const knex = require("knex")({
         host: "cougar-cruiser.postgres.database.azure.com",
         user: "admin403",
         password: "nq4yML^boKwZQCwG",
+        database: "cougar_cruiser",
         port: 5432
     }
 });
+
+function formatDate(dateString) {
+    // Parse the date string into a JavaScript Date object
+    const date = new Date(dateString);
+
+    // Check if the parsed date is valid
+    if (isNaN(date.getTime())) {
+        return 'Invalid Date';
+    }
+
+    // Format the date
+    return date.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+    });
+}
+
+function formatTime(timeString) {
+    const [hours, minutes] = timeString.split(':');
+    let formattedTime;
+
+    try {
+        const parsedHours = parseInt(hours, 10);
+        const period = parsedHours >= 12 ? 'PM' : 'AM';
+        const formattedHours = parsedHours % 12 || 12; // Convert 0 to 12 for 12-hour format
+        formattedTime = `${formattedHours}:${minutes} ${period}`;
+    } catch (error) {
+        formattedTime = 'Invalid Time';
+    }
+
+    return formattedTime;
+}
+
+
 
 //Changed port to specify what environment our application will be running on
 const port = process.env.PORT || 3000;
@@ -28,7 +64,36 @@ app.get("/", (req, res) => res.render("index"));
 app.get("/login", (req, res) =>
     res.render("login"))
 
-app.get("/ride", (req, res) =>
-    res.render("rideDetails"))
+app.get("/rides", (req, res) => {
+    knex.select(
+        'r.ride_id',
+        'r.start_state',
+        'r.start_city',
+        'r.start_zip',
+        'r.student_driver',
+        'r.time_leaving',
+        'r.date_leaving',
+        'r.end_city',
+        'r.end_state',
+        'r.end_zip',
+        'r.max_students',
+        knex.raw('COUNT(sr.student_id) AS current_students'),
+        knex.raw('(r.max_students - COUNT(sr.student_id) - 1) AS spots_remaining')
+    )
+        .from('ride as r')
+        .leftJoin('student_ride as sr', 'r.ride_id', 'sr.ride_id')
+        .groupBy('r.ride_id')
+        .having('r.max_students', '>', knex.raw('COUNT(sr.student_id)')).then(rides => {
+            const formattedRides = rides.map(ride => ({
+                ...ride,
+                formattedDateLeaving: formatDate(ride.date_leaving),
+                formattedTimeLeaving: formatTime(ride.time_leaving)
+            }));
+            console.log(formattedRides);
+            res.render("rideDetails", { allRides: formattedRides });
+        })
+
+}
+)
 
 app.listen(port, () => console.log("Server is running"))
